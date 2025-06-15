0265c942c88a77cf021b6651964356999e4a0e4f
arisaEN <hkyamaha234@gmail.com>
2025-06-15 10:48:09 +0900

    プロジェクト ファイルを追加します。

---
+++ UHFtest/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace UHFtest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_test = new System.Windows.Forms.Button();
+            this.lB_test = new System.Windows.Forms.ListBox();
+            this.btn_ReadMultipleEPC = new System.Windows.Forms.Button();
+            this.lB_Continuous = new System.Windows.Forms.ListBox();
+            this.btn_StartScan = new System.Windows.Forms.Button();
+            this.btn_StopScan = new System.Windows.Forms.Button();
+            this.lV_Continuous = new System.Windows.Forms.ListView();
+            this.cb_AllowDuplicates = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // btn_test
+            // 
+            this.btn_test.Location = new System.Drawing.Point(680, 113);
+            this.btn_test.Name = "btn_test";
+            this.btn_test.Size = new System.Drawing.Size(114, 32);
+            this.btn_test.TabIndex = 0;
+            this.btn_test.Text = "Test";
+            this.btn_test.UseVisualStyleBackColor = true;
+            this.btn_test.Click += new System.EventHandler(this.btn_test_Click);
+            // 
+            // lB_test
+            // 
+            this.lB_test.FormattingEnabled = true;
+            this.lB_test.ItemHeight = 12;
+            this.lB_test.Location = new System.Drawing.Point(13, 19);
+            this.lB_test.Name = "lB_test";
+            this.lB_test.Size = new System.Drawing.Size(163, 364);
+            this.lB_test.TabIndex = 1;
+            // 
+            // btn_ReadMultipleEPC
+            // 
+            this.btn_ReadMultipleEPC.Location = new System.Drawing.Point(680, 184);
+            this.btn_ReadMultipleEPC.Name = "btn_ReadMultipleEPC";
+            this.btn_ReadMultipleEPC.Size = new System.Drawing.Size(115, 32);
+            this.btn_ReadMultipleEPC.TabIndex = 2;
+            this.btn_ReadMultipleEPC.Text = "Read multiple EPC";
+            this.btn_ReadMultipleEPC.UseVisualStyleBackColor = true;
+            this.btn_ReadMultipleEPC.Click += new System.EventHandler(this.btn_ReadMultipleEPC_Click);
+            // 
+            // lB_Continuous
+            // 
+            this.lB_Continuous.FormattingEnabled = true;
+            this.lB_Continuous.ItemHeight = 12;
+            this.lB_Continuous.Location = new System.Drawing.Point(182, 19);
+            this.lB_Continuous.Name = "lB_Continuous";
+            this.lB_Continuous.Size = new System.Drawing.Size(51, 364);
+            this.lB_Continuous.TabIndex = 3;
+            // 
+            // btn_StartScan
+            // 
+            this.btn_StartScan.Location = new System.Drawing.Point(680, 252);
+            this.btn_StartScan.Name = "btn_StartScan";
+            this.btn_StartScan.Size = new System.Drawing.Size(124, 23);
+            this.btn_StartScan.TabIndex = 4;
+            this.btn_StartScan.Text = "btn_StartScan";
+            this.btn_StartScan.UseVisualStyleBackColor = true;
+            this.btn_StartScan.Click += new System.EventHandler(this.btn_StartScan_Click);
+            // 
+            // btn_StopScan
+            // 
+            this.btn_StopScan.Location = new System.Drawing.Point(679, 310);
+            this.btn_StopScan.Name = "btn_StopScan";
+            this.btn_StopScan.Size = new System.Drawing.Size(125, 23);
+            this.btn_StopScan.TabIndex = 5;
+            this.btn_StopScan.Text = "btn_StopScan";
+            this.btn_StopScan.UseVisualStyleBackColor = true;
+            this.btn_StopScan.Click += new System.EventHandler(this.btn_StopScan_Click);
+            // 
+            // lV_Continuous
+            // 
+            this.lV_Continuous.AllowColumnReorder = true;
+            this.lV_Continuous.HideSelection = false;
+            this.lV_Continuous.Location = new System.Drawing.Point(239, 19);
+            this.lV_Continuous.Name = "lV_Continuous";
+            this.lV_Continuous.Size = new System.Drawing.Size(403, 364);
+            this.lV_Continuous.TabIndex = 6;
+            this.lV_Continuous.UseCompatibleStateImageBehavior = false;
+            // 
+            // cb_AllowDuplicates
+            // 
+            this.cb_AllowDuplicates.AutoSize = true;
+            this.cb_AllowDuplicates.Location = new System.Drawing.Point(662, 19);
+            this.cb_AllowDuplicates.Name = "cb_AllowDuplicates";
+            this.cb_AllowDuplicates.Size = new System.Drawing.Size(142, 16);
+            this.cb_AllowDuplicates.TabIndex = 7;
+            this.cb_AllowDuplicates.Text = "連続検出切替(テスト用)";
+            this.cb_AllowDuplicates.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(816, 404);
+            this.Controls.Add(this.cb_AllowDuplicates);
+            this.Controls.Add(this.lV_Continuous);
+            this.Controls.Add(this.btn_StopScan);
+            this.Controls.Add(this.btn_StartScan);
+            this.Controls.Add(this.lB_Continuous);
+            this.Controls.Add(this.btn_ReadMultipleEPC);
+            this.Controls.Add(this.lB_test);
+            this.Controls.Add(this.btn_test);
+            this.Name = "Form1";
+            this.RightToLeftLayout = true;
+            this.Text = "UHFtest";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_test;
+        private System.Windows.Forms.ListBox lB_test;
+        private System.Windows.Forms.Button btn_ReadMultipleEPC;
+        private System.Windows.Forms.ListBox lB_Continuous;
+        private System.Windows.Forms.Button btn_StartScan;
+        private System.Windows.Forms.Button btn_StopScan;
+        private System.Windows.Forms.ListView lV_Continuous;
+        private System.Windows.Forms.CheckBox cb_AllowDuplicates;
+    }
+}
+
---
+++ UHFtest/Form1.cs
@@ -0,0 +1,335 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+
+namespace UHFtest
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("E7umf.DLL", EntryPoint = "uhf_connect")]
+        public static extern Int32 uhf_connect(Int16 port, Int32 baud);
+        [DllImport("E7umf.DLL", EntryPoint = "uhf_disconnect")]
+        public static extern Int32 uhf_disconnect(Int32 icdev);
+        [DllImport("E7umf.DLL", EntryPoint = "uhf_read")]
+        public static extern Int32 uhf_read(Int32 icdev, byte infoType, Int32 address, Int32 rlen, byte[] pDataR);
+        [DllImport("E7umf.DLL", EntryPoint = "uhf_write")]
+        public static extern Int32 uhf_write(Int32 icdev, byte infoType, Int32 address, Int32 wlen, byte[] pDataW);
+        [DllImport("E7umf.DLL", EntryPoint = "uhf_action")]
+        public static extern Int32 uhf_action(Int32 icdev, byte action, byte time);
+        [DllImport("E7umf.DLL", EntryPoint = "uhf_inventory")]
+        public static extern Int32 uhf_inventory(Int32 icdev, Int32[] tagCount, Int32[] datalen, byte[] pDataR);
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_test_Click(object sender, EventArgs e)
+        {
+            Int32 deviceHandle = -1;
+            Int32 st;
+            byte infoType;
+            Int32 address, length;
+            byte[] bufDataR = new byte[1024];
+
+            //data to write: 11223344556677889900AABBCCDDEEFF
+            byte[] bufDataW = { 0x31, 0x31, 0x32, 0x32, 0x33, 0x33, 0x34, 0x34, 0x35, 0x35, 0x36, 0x36, 0x37, 0x37, 0x38, 0x38, 0x39, 0x39, 0x30, 0x30, 0x41, 0x41, 0x42, 0x42, 0x43, 0x43, 0x44, 0x44, 0x45, 0x45, 0x46, 0x46 };
+
+            //connect reader
+            deviceHandle = uhf_connect(100, 115200);//100->USB port, other value serial port; 115200->serial port baud rate
+            if (-1 == deviceHandle)
+            {
+                lB_test.Items.Add("Connect reader failed！");
+                return;
+            }
+            lB_test.Items.Add("Connect reader ok！");
+
+
+            //read EPC
+            infoType = 1; //1：EPC，2：TIP，3：USER，0：reserved
+            address = 0;
+            length = 8;  //will get 32 bytes data
+            st = uhf_read(deviceHandle, infoType, address, length, bufDataR);
+            if (st != 0) lB_test.Items.Add("read EPC Error!");
+            else
+            {
+                lB_test.Items.Add("Read EPC OK:");
+                lB_test.Items.Add(System.Text.Encoding.Default.GetString(bufDataR));
+            }
+
+
+            //read TID
+            infoType = 2; //1：EPC，2：TIP，3：USER，0：reserved
+            address = 0;
+            length = 8;  //will get 32 bytes data
+            st = uhf_read(deviceHandle, infoType, address, length, bufDataR);
+            if (st != 0) lB_test.Items.Add("read TID Error!");
+            else
+            {
+                lB_test.Items.Add("Read TID OK:");
+                lB_test.Items.Add(System.Text.Encoding.Default.GetString(bufDataR));
+            }
+
+
+            //read USER
+            infoType = 3; //1：EPC，2：TIP，3：USER，0：reserved
+            address = 0;
+            length = 8;  //will get 32 bytes data
+            st = uhf_read(deviceHandle, infoType, address, length, bufDataR);
+            if (st != 0) lB_test.Items.Add("read USER Error!");
+            else
+            {
+                lB_test.Items.Add("Read USER OK:");
+                lB_test.Items.Add(System.Text.Encoding.Default.GetString(bufDataR));
+            }
+
+
+            //read reserved
+            infoType = 0; //1：EPC，2：TIP，3：USER，0：reserved
+            address = 0;
+            length = 4;  //will get 16 bytes data
+            st = uhf_read(deviceHandle, infoType, address, length, bufDataR);
+            if (st != 0) lB_test.Items.Add("read reserved Error!");
+            else
+            {
+                lB_test.Items.Add("Read reserved OK:");
+                lB_test.Items.Add(System.Text.Encoding.Default.GetString(bufDataR));
+            }
+
+            /*
+                        //write EPC
+                        infoType = 1; //1：EPC，2：TIP，3：USER，0：reserved
+                        address = 2;
+                        length = 6;  //will write 24 bytes data
+                        st = uhf_write(deviceHandle, infoType, address, length, bufDataW);
+                        if (st != 0) listBox1.Items.Add("Write EPC Error!");
+                        else
+                        {
+                            listBox1.Items.Add("Write EPC OK:");
+                            bufDataW[24] = 0;
+                            listBox1.Items.Add(System.Text.Encoding.Default.GetString(bufDataW));
+                        }
+
+
+                        //write USER
+                        infoType = 3; //1：EPC，2：TIP，3：USER，0：reserved
+                        address = 0;
+                        length = 4;  //will write 16 bytes data
+                        st = uhf_write(deviceHandle, infoType, address, length, bufDataW);
+                        if (st != 0) listBox1.Items.Add("Write USER Error!");
+                        else
+                        {
+                            listBox1.Items.Add("Write USER OK:");
+                            bufDataW[16] = 0;
+                            listBox1.Items.Add(System.Text.Encoding.Default.GetString(bufDataW));
+                        }
+
+
+                        //write reserved
+                        infoType = 0; //1：EPC，2：TIP，3：USER，0：reserved
+                        address = 0;
+                        length = 4;  //will write 16 bytes data
+                        st = uhf_write(deviceHandle, infoType, address, length, bufDataW);
+                        if (st != 0) listBox1.Items.Add("Write reserved Error!");
+                        else
+                        {
+                            listBox1.Items.Add("Write reserved OK:");
+                            bufDataW[16] = 0;
+                            listBox1.Items.Add(System.Text.Encoding.Default.GetString(bufDataW));
+                        }
+            */
+
+            //reader action
+            //action:  beep:0x01, red led on:0x02, green led on:0x04, yellow led on:0x08
+            //time: Unit:10ms
+            st = uhf_action(deviceHandle, (0x01 | 0x04), 50);  //beep and green led on 500ms
+            if (st != 0) lB_test.Items.Add("Reader action Error!");
+            else
+            {
+                lB_test.Items.Add("Reader action OK!");
+            }
+
+
+            uhf_disconnect(deviceHandle); //disconnect reader
+            lB_test.Items.Add("Disconnect reader！");
+
+        }
+
+        private void btn_ReadMultipleEPC_Click(object sender, EventArgs e)
+        {
+            Int32 deviceHandle = -1;
+            Int32 st, i;
+            Int32 offset = 0;
+            Int32 EPCcount;
+            Int32[] tagCount = new Int32[2];
+            Int32[] length = new Int32[2];
+            byte[] bufDataR = new byte[2056];
+            byte[] bufEPC = new byte[256];
+            string str;
+
+            //connect reader
+            deviceHandle = uhf_connect(100, 115200);//100->USB port, other value serial port; 115200->serial port baud rate
+            if (-1 == deviceHandle)
+            {
+                lB_test.Items.Add("Connect reader failed！");
+                return;
+            }
+            lB_test.Items.Add("Connect reader ok！");
+
+            //read multiple EPC
+            //bufDataR: Data1 length(1 byte) + EPC1Read count(1byte) + EPC1(length=Data1 length-1), Data2 length(1 byte) + EPC2Read count(1byte) + EPC2(length=Data1 length-1)...
+            st = uhf_inventory(deviceHandle, tagCount, length, bufDataR);
+            if (st != 0) lB_test.Items.Add("Read Multiple EPC Error!");
+            else
+            {
+                lB_test.Items.Add("Read Multiple EPC OK!");
+                lB_test.Items.Add("Tag count::");
+                lB_test.Items.Add(tagCount[0].ToString());
+
+                EPCcount = tagCount[0];
+                while (EPCcount > 0)
+                {
+                    for (i = 0; i < bufDataR[offset] - 1; i++)
+                        bufEPC[i] = bufDataR[offset + 2 + i];
+                    str = String.Concat("read count:", bufDataR[offset + 1].ToString(), ", EPC: ", System.Text.Encoding.Default.GetString(bufEPC));
+                    lB_test.Items.Add(str);
+                    EPCcount--;
+                    offset += bufDataR[offset] + 1;
+                }
+            }
+
+            uhf_disconnect(deviceHandle); //disconnect reader
+            lB_test.Items.Add("Disconnect reader！");
+
+        }
+
+        //2025/6/14追加分
+
+
+        // 連続スキャン用のタイマーと接続ハンドル
+        private System.Windows.Forms.Timer scanTimer = new System.Windows.Forms.Timer();
+        private Int32 persistentDeviceHandle = -1;
+        private Dictionary<string, DateTime> epcLastSeen = new Dictionary<string, DateTime>();
+
+        // フォームロード時にタイマー設定とListView初期化
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            scanTimer.Interval = 500; // 0.5秒ごとにスキャン
+            scanTimer.Tick += ScanTimer_Tick;
+
+            // ListView列の定義（2列：時刻, ラベル）
+            lV_Continuous.View = View.Details;
+            lV_Continuous.FullRowSelect = true;
+            lV_Continuous.GridLines = true;
+
+            
+            lV_Continuous.Columns.Add("タグ", 120);
+            lV_Continuous.Columns.Add("時刻 (JST)", 200);
+        }
+
+        // スキャン開始ボタン
+        private void btn_StartScan_Click(object sender, EventArgs e)
+        {
+            if (persistentDeviceHandle == -1)
+            {
+                persistentDeviceHandle = uhf_connect(100, 115200);
+                if (persistentDeviceHandle == -1)
+                {
+                    MessageBox.Show("リーダー接続失敗！");
+                    return;
+                }
+            }
+
+            scanTimer.Start();
+        }
+
+        // スキャン停止ボタン
+        private void btn_StopScan_Click(object sender, EventArgs e)
+        {
+            scanTimer.Stop();
+
+            if (persistentDeviceHandle != -1)
+            {
+                uhf_disconnect(persistentDeviceHandle);
+                persistentDeviceHandle = -1;
+            }
+        }
+
+        // タイマーごとの読み取り処理
+        private void ScanTimer_Tick(object sender, EventArgs e)
+        {
+            Int32[] tagCount = new Int32[2];
+            Int32[] length = new Int32[2];
+            byte[] bufDataR = new byte[2056];
+            byte[] bufEPC = new byte[256];
+            int offset = 0;
+
+            int st = uhf_inventory(persistentDeviceHandle, tagCount, length, bufDataR);
+            if (st != 0)
+            {
+                return;
+            }
+
+            int EPCcount = tagCount[0];
+
+            while (EPCcount > 0)
+            {
+                for (int i = 0; i < bufDataR[offset] - 1; i++)
+                    bufEPC[i] = bufDataR[offset + 2 + i];
+
+                string epc = BitConverter.ToString(bufEPC, 0, bufDataR[offset] - 1).Replace("-", "");
+
+                bool allowDisplay = true;
+
+                if (!cb_AllowDuplicates.Checked)
+                {
+                    if (epcLastSeen.ContainsKey(epc))
+                    {
+                        DateTime lastSeen = epcLastSeen[epc];
+                        if ((DateTime.Now - lastSeen).TotalMinutes < 10)
+                        {
+                            allowDisplay = false; // ✋ 10分以内 → 表示しない
+                        }
+                    }
+
+                    // 最終検出時刻を更新（検出された時点で記録）
+                    epcLastSeen[epc] = DateTime.Now;
+                }
+
+                if (allowDisplay)
+                {
+                    // 音＋LED（表示されたときだけ鳴らす）
+                    uhf_action(persistentDeviceHandle, 0x01 | 0x04, 30);
+
+                    string label = GetTagLabel_StaffIdentityCard(epc);
+                    string timestamp = DateTime.Now.ToString("HH:mm:ss");
+
+                    var item = new ListViewItem(timestamp);
+                    item.SubItems.Add(label);
+                    lV_Continuous.Items.Add(item);
+                    lV_Continuous.EnsureVisible(lV_Continuous.Items.Count - 1);
+                }
+
+                EPCcount--;
+                offset += bufDataR[offset] + 1;
+            }
+        }
+
+
+        // EPCをラベルに変換（必要なら複数パターン追加可）
+        private string GetTagLabel_StaffIdentityCard(string epc)
+        {
+            if (epc == "303042303741313530383046373441463845303430383141")
+                return "社員証";
+            return epc;
+        }
+    }
+}
